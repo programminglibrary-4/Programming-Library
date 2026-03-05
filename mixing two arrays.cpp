1e90158c72455fe4e0fa9c9e17b3e5e6ad89615b
programminglibrary-4 <programminglibrary4@gmail.com>
2026-03-05 05:45:19 +0300

    Update mixing two arrays.cpp

--- mixing two arrays.cpp
+++ mixing two arrays.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 
-void mix_array(int array0[12] , int array1[6], int array2[6])
+void mix_array(int array0[268435455], int array0_size , int array1[268435455], int array_size , int array2[268435455] )
 {
 
-    int new_array[12];
+   
 
     int for_counter;
     for_counter = 0;
@@ -15,16 +15,16 @@ void mix_array(int array0[12] , int array1[6], int array2[6])
     int temp;
     temp = 0;
 
-    for (for_counter; for_counter < 6; for_counter++)
+    for (for_counter; for_counter < array_size ; for_counter++)
     {
 
         array0[for_counter] = array1[for_counter];
     }
 
-    for (for_counter2, temp; for_counter2 < 12; for_counter2++, temp++)
+    for (for_counter, temp; for_counter <= array0_size ; for_counter++, temp++)
     {
 
-        array0[for_counter2] = array2[temp];
+        array0[for_counter] = array2[temp];
     }
    
 }
@@ -32,15 +32,18 @@ void mix_array(int array0[12] , int array1[6], int array2[6])
 
 
 
---- ملاحضة ---
+//* --- ملاحضة ---
 //^ الدالة تدمج مصفوفتين معا
 
+//* --- طريقة الاستعمال ---
+/*
+
+1. ادخل الدالة التي ستضع بداخلها الدالتين معا
+2. اخل حجم تلك الدالة
+3. ادخل المصفوفه الاولى
+4. ادخل حجم المصفوفة الاولى
+5. ادخل المصفوفة الثانية 
 
 
-
-
-
-
-
-
+*/
 
