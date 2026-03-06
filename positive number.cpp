85cbd710a95042ea1f862273ee994ba3f324152d
programminglibrary-4 <programminglibrary4@gmail.com>
2026-03-06 14:16:04 +0300

    Add files via upload

---
+++ positive number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// دالة لطباعة الاعداد الموجبة الفردية و الزوجية 
+void Positive_Number(int arr[], int size)
+{
+    cout << "Positive numbers: ";
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > 0)
+            cout << arr[i] << " ";
+    }
+    cout << endl;
+}
