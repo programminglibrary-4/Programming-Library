a379d28a731dd9471080fd099aa094957a8e6857
programminglibrary-4 <programminglibrary4@gmail.com>
2026-03-06 14:17:58 +0300

    Change condition to include zero in positive numbers

--- positive number.cpp
+++ positive number.cpp
@@ -7,8 +7,9 @@ void Positive_Number(int arr[], int size)
     cout << "Positive numbers: ";
     for (int i = 0; i < size; i++)
     {
-        if (arr[i] > 0)
+        if (arr[i] >= 0)
             cout << arr[i] << " ";
     }
     cout << endl;
 }
+
